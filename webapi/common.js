709b34a2eec327d90303dd8dba1d587d1d08d2d4
tthtnt <2420313474@qq.com>
2018-08-04 17:32:59 +0800

    第二天

--- webapi/common.js
+++ webapi/common.js
@@ -1,3 +1,29 @@
 function my$(id){
     return document.getElementById(id);
 }
+
+//设置标签文本内容
+function setInnerText(element,text){
+    //判断浏览器是否支持这个属性
+    if(typeof element.textContent == "undefined"){
+        //不支持
+        element.innerText = text;
+    }else{
+        //支持
+        element.textContent = text;
+    }
+}
+
+
+//获取标签中间的文本内容
+
+function getInnerText(element){
+    //
+    if(typeof element.textContent == "undefined"){
+        //不支持
+        return element.innerText;
+    }else{
+        //支持
+        return element.textContent;
+    }
+}
