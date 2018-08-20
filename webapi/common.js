80d32377dd29c341b8c7408e12c83640ae5b34ac
tthtnt <2420313474@qq.com>
2018-08-20 15:56:21 +0800

    第四天接近结束

--- webapi/common.js
+++ webapi/common.js
@@ -27,3 +27,26 @@ function getInnerText(element){
         return element.textContent;
     }
 }
+
+//添加绑定事件兼容代码
+function addEventListener(element, type, fn) {
+    if (element.addEventListener){
+        element.addEventListener(type,fn, false);
+    }else if (element.attachEvent) {
+        element.attachEvent("on"+type,fn);
+    }else {
+        element["on"+type] = fn;
+    }
+}
+
+//解绑事件的兼容代码
+
+function removeEventListener(element, type, fn) {
+    if (element.removeEventListener){
+        element.removeEventListener(type,fn, false);
+    }else if (element.detachEvent) {
+        element.detachEvent("on"+type,fn);
+    }else {
+        element["on"+type] = null;
+    }
+}
