a6bd0c61a3d56347d08d3f77744f5ffef6a0bd20
tthtnt <2420313474@qq.com>
2018-08-23 16:57:55 +0800

    第七天结束

--- webapi/common.js
+++ webapi/common.js
@@ -27,6 +27,21 @@ function getInnerText(element){
         return element.textContent;
     }
 }
+/*获取父级元素中的第一个子元素
+* @param element 父级元素
+* @returns {*} 父级中的子级元素
+* */
+function getFirstElement(element) {
+    if (element.firstElementChild) {
+        return element.firstElementChild;
+    }else {
+        var node = element.firstChild;
+        while (node && node.nodeType != 1) {
+            node = node.nextSibling;
+        }
+        return node;
+    }
+}
 
 //添加绑定事件兼容代码
 function addEventListener(element, type, fn) {
@@ -72,14 +87,29 @@ function changeAnimate(element,json,fn) {
     element.timeId = setInterval(function () {
         var flag = true;
         for (var attr in json) {
-            var current = parseInt(getStyle(element,attr));
-            var target = json[attr];
-            var step = (target - current)/10;
-            step = step > 0 ? Math.ceil(step):Math.floor(step);
-            current += step;
-            element.style[attr] = current + "px";
-            if (current != target){
-                flag = false;
+            if (attr == "opacity") {
+                var current = getStyle(element,attr)*100;
+                var target = json[attr]*100;
+                var step = (target - current)/10;
+                step = step > 0 ? Math.ceil(step):Math.floor(step);
+                current += step;
+                element.style[attr] = current/100;
+                if (current != target){
+                    flag = false;
+                }
+
+            }else if (attr == "zIndex") {
+                element.style[attr] = json[attr];
+            }else {
+                var current = parseInt(getStyle(element,attr));
+                var target = json[attr];
+                var step = (target - current)/10;
+                step = step > 0 ? Math.ceil(step):Math.floor(step);
+                current += step;
+                element.style[attr] = current + "px";
+                if (current != target){
+                    flag = false;
+                }
             }
         }
         if (flag) {
@@ -104,3 +134,29 @@ function getStyle(element, attr) {
     //判断浏览器是否支持这个方法
     return window.getComputedStyle ? window.getComputedStyle(element,null)[attr] : element.currentStyle[attr];
 }
+//获取e
+var evt = {
+    getEvent:function (e) {
+        return window.event || e;
+    },
+    getClientX:function (e) {
+        return this.getEvent(e).clientX;
+    },
+    getClientY:function (e) {
+        return this.getEvent(e).clientY;
+    },
+    getScrollLeft:function () {
+        return window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft||0;
+    },
+    getScrollTop:function () {
+        return window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop||0;
+    },
+    //相对于页面的横坐标
+    getPageX:function (e) {
+        return this.getEvent(e).pageX?this.getEvent(e).pageX:this.getClientX(e)+this.getScrollLeft();
+    },
+    //相对于页面的纵坐标
+    getPageY:function (e) {
+        return this.getEvent(e).pageY?this.getEvent(e).pageY:this.getClientY(e)+this.getScrollTop();
+    },
+};
